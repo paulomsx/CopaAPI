8fcbfdaa31946a744df89d38833ed7c13368c134
paulomsx <paulo.bola2@gmail.com>
2017-12-31 18:38:19 -0200

    subir

--- CopaAPI/Controllers/CopaController.cs
+++ CopaAPI/Controllers/CopaController.cs
@@ -15,14 +15,29 @@ namespace CopaAPI.Controllers
         {
             try
             {
-                Usuarios usuario = new Usuarios();
-                usuario.nome = "dirce";
-                usuario.email = "dirce@gmail.com";
-            
-                Contexto contexto = new Contexto();
 
-                contexto.usuarios.Add(usuario);
-                contexto.SaveChanges();
+                using (var contexto = new Contexto()) {
+
+                    var query = from a in contexto.usuarios
+                                where a.email.Contains("zxc")
+                                select a;
+
+                    if (query.Count() > 0) {
+                        var r = "nao é nulo";
+                    }
+
+                    foreach (var usuario in query)
+                    {
+                        var nome = usuario.nome;
+                    }
+                    
+
+                }
+
+                //    Contexto contexto = new Contexto();
+
+                //contexto.usuarios.Add(usuario);
+                //contexto.SaveChanges();
 
                 return Request.CreateResponse(HttpStatusCode.OK, new string[] { "value1", "value2" }); ;
 
@@ -35,4 +50,34 @@ namespace CopaAPI.Controllers
             }
         }
     }
+
+
+
+    //public HttpResponseMessage Get()
+    //{
+    //    try
+    //    {
+    //        Usuarios usuario = new Usuarios();
+    //        usuario.nome = "dirce";
+    //        usuario.email = "dirce@gmail.com";
+
+    //        Contexto contexto = new Contexto();
+
+    //        contexto.usuarios.Add(usuario);
+    //        contexto.SaveChanges();
+
+    //        return Request.CreateResponse(HttpStatusCode.OK, new string[] { "value1", "value2" }); ;
+
+    //    }
+    //    catch (KeyNotFoundException)
+    //    {
+    //        string mensagem = string.Format("Não encontrado {0}", 1);
+    //        HttpError error = new HttpError(mensagem);
+    //        return Request.CreateResponse(HttpStatusCode.NotFound, error);
+    //    }
+    //}
+//}   
+
+   
+
 }
