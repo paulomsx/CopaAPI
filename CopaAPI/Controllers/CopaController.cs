df33b78ad49091a696522a7bfd54c8106e3868aa
paulomsx <paulo.bola2@gmail.com>
2018-01-04 00:06:19 -0200

    excluir pasta obj

--- CopaAPI/Controllers/CopaController.cs
+++ CopaAPI/Controllers/CopaController.cs
@@ -13,22 +13,55 @@ namespace CopaAPI.Controllers
 
         public HttpResponseMessage Post([FromBody] Usuarios usuario) {
 
-            using (var contexto = new Contexto())
+            try
             {
-                var query = from a in contexto.usuarios
-                            where a.email.Contains(usuario.email)
+                using (var contexto = new Contexto())
+                {
+                    string email = usuario.email;
+                    string nome = usuario.nome;
+
+                    var query = from a in contexto.usuarios
+                                select a;
+
+                    // Checagem do email
+                    query = from a in contexto.usuarios
+                            where a.email == email
                             select a;
 
-                //Verifica se existe email cadastrado
-                if (query.Count() == 0)
-                {
+                    if (query.Count() > 0) {
+                        return Request.CreateResponse(HttpStatusCode.Conflict, "email já cadastrado");
+                    }
+
+                    // Checagem do nome
+                    query = from a in contexto.usuarios
+                            where a.nome == nome
+                            select a;
+
+                    if (query.Count() > 0)                {
+                        return Request.CreateResponse(HttpStatusCode.Conflict, "nome do usuário já cadastrado");
+                    }
+
+                    //Insere usuário
                     contexto.usuarios.Add(usuario);
                     contexto.SaveChanges();
-                    return Request.CreateResponse(HttpStatusCode.OK);
+
+                    // Pega id do usuario
+                    query = from a in contexto.usuarios
+                            where a.email == email
+                            select a;
+
+                    
+                    int id = query.FirstOrDefault().id;
+                    
+                    return Request.CreateResponse(HttpStatusCode.OK, string.Format("id = {0}",id));
+
                 }
-                else {
-                    return Request.CreateResponse(HttpStatusCode.Conflict,"email já cadastrado");
-                }
+            }
+            catch (KeyNotFoundException)
+            {
+                string mensagem = string.Format("Erro {0}", 1);
+                HttpError error = new HttpError(mensagem);
+                return Request.CreateResponse(HttpStatusCode.NotFound, error);
             }
         }
 
@@ -41,18 +74,22 @@ namespace CopaAPI.Controllers
 
                 using (var contexto = new Contexto()) {
 
-                    var query = from a in contexto.usuarios
-                                where a.email.Contains("zxc")
-                                select a;
 
-                    if (query.Count() > 0) {
-                        var r = "nao é nulo";
-                    }
+                    string email = "ovo@gmail.com";
+
+                    var query = contexto.usuarios.Where(a => a.email == email);
+
 
                     foreach (var usuario in query)
                     {
                         var nome = usuario.nome;
                     }
+
+                    //var query = from a in contexto.usuarios
+                    //            where a.email.Contains(email)
+                    //            select a;
+                    // var query = contexto.usuarios.Where(a => a.email == email);
+                    //List<Usuarios> usu = query.ToList();
 
 
                 }
